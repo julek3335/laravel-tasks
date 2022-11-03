fe0f8aee8ddfc9ed0d0d3532d3313b4e628c2d81
julek3335 <86291866+julek3335@users.noreply.github.com>
2022-11-03 17:06:21 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::controller(DashboardController::class)->group(function () {
 
 Route::controller(VehicleController::class)->group(function () {
     Route::post('/vehicles/delete/{vehicle_id}', 'delete')->middleware(['auth'])->name('vehiclesDelete');;
-    Route::get('/vehicles', 'showAll')->middleware(['auth'])->name('showAllVehicles');;
+    Route::get('/vehicles', 'showAll')->name('showAllVehicles');;
     Route::get('/vehicles/{id}', 'show')->middleware(['auth'])->name('dashboard');;
     Route::get('/vehicle/add', function () {
         return view('vehicle.add');
@@ -66,7 +66,7 @@ Route::controller(UserController::class)->group(function () {
     Route::get('/users/delete/{user_id}', 'delete')->middleware(['auth'])->name('deleteUser');
     Route::get('/user/add', 'prepareAdd')->middleware(['auth'])->name('dashboard');
     Route::post('/user/add', 'store')->middleware(['auth'])->name('dashboard');
-    Route::get('/users', 'showAll')->middleware(['auth'])->name('showAllUsers');
+    Route::get('/users', 'showAll')->name('showAllUsers');
     Route::get('/user/{id}', 'show')->middleware(['auth'])->name('dashboard');
     Route::put('/user/edit/{id}', [UserController::class, 'updateUser']);
     Route::get('/user/edit/{id}', 'userToEdit')->middleware(['auth'])->name('dashboard');
